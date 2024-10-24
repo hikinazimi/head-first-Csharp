881be717ee23915e9a5e3e66f0ed8f789ba3e17c
hikinazimi <2427646226@qq.com>
2024-10-24 21:43:39 +0800

    chapter-8

---
+++ code/chapter-8/Duck/Duck/DuckComparerBySize.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Duck
+{
+    class DuckComparerBySize : IComparer<Duck>
+    {
+        public int Compare(Duck x, Duck y)
+        {
+            if (x.Size < y.Size)
+                return -1;
+            if (x.Size > y.Size)
+                return 1;
+            return 0;
+        }
+    }
+}
---
+++ code/chapter-8/Duck/Duck/KindOfDuck.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Duck
+{
+
+    enum KindOfDuck
+    {
+        Mallard,
+        Muscovy,
+        Loon,
+    }
+
+}
